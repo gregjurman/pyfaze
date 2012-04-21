f5aa16ff5d5e7b3d2ba6bca225a118e7b90e234f
Greg Jurman <gdj2214@rit.edu>
2012-04-20 21:06:02 -0400

    Added some aliases

--- pyfaze/types.py
+++ pyfaze/types.py
@@ -2,7 +2,12 @@ from struct import pack, unpack, calcsize
 from functools import partial
 from collections import namedtuple
 
-__all__ = ["ANA_REG", "ANA_UI", "ANA_SI", "ANA_UC", "ANA_SC"]
+__all__ = [ 'make_anafaze_type',
+    "ANA_REG","ANA_BYTE", 
+    "ANA_DIGOUT", "ANA_DIGIN", 
+    "ANA_UI", "ANA_SI", 
+    "ANA_UC", "ANA_SC"
+]
 
 
 def make_anafaze_type(struct_def, name):
@@ -17,5 +22,8 @@ def make_anafaze_type(struct_def, name):
 
 ANA_REG = ANA_UI = make_anafaze_type("H", "ANA_UI") # Unsigned Integer (16-bit unsigned int)
 ANA_SI = make_anafaze_type("h", "ANA_SI") # Signed Integer (16-bit signed int)
-ANA_UC = make_anafaze_type("B", "ANA_UC") # Unsigned Char (8-bit unsigned char)
+ANA_BYTE = ANA_DIGIN = ANA_UC = make_anafaze_type("B", "ANA_UC") # Unsigned Char (8-bit unsigned char)
 ANA_SC = make_anafaze_type("b", "ANA_SC") # Signed Char (8-bit signed char)
+ANA_DIGOUT = make_anafaze_type("BBBBBBBB", "ANA_DIGOUT")
+
+
